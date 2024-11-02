a43a09a169af9497626567181fa766f52b15ef42
Itpalyer <3129792806@qq.com>
2024-11-02 23:14:29 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -23,7 +23,7 @@ requests.urllib3.disable_warnings()
 UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26'
 
 # 初始化环境变量开头
-cs = 0
+cs = 2
 ZData = "5"
 ttoken = ""
 tuserid = ""
